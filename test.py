27a9133452c480180b95a884dbcfecb1a93fa17b
Sleiman Ghanem <you@example.com>
2023-03-22 17:12:11 +0200

    added test for subdomain function

--- test.py
+++ test.py
@@ -1,13 +1,26 @@
 import requests
 import sys
 
-def request(url):
-  try:
-    return requests.get("http://" + url)
-  except requests.exceptions.ConnectionError:
-    try: 
-      return requests.get("https://" + url)
-    except requests.exceptions.ConnectionError:
-      pass
-    
 
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        try:
+            return requests.get("https://" + url)
+        except requests.exceptions.ConnectionError:
+            pass
+
+
+def getSubdomains(url):
+    with open("subdomains_dictionary.bat") as file:
+        output = open("subdomins_output.bat", "w")
+        for line in file:
+            word = line.strip()
+            target = word + "." + url
+            response = request(target)
+            if response:
+                print(target)
+                output.write(target + "\n")
+        output.close()
+        
